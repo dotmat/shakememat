381be995beada231b61df4df0840f96acad2a7df
Mathew <mathew@mathewjenkinson.co.uk>
2021-12-02 07:54:18 -0600

    Add files via upload
    Blank config file

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "port":3333, // Port the App will run on
+    "AppURL": "", // The root URL of the apps location
+    "AppName":"ShakeMeMat", // App Name 'ShakeMeMat' 
+    "AppLogoURL":"/images/brand/ShakeMeMat.png", // Logo of the App
+    "AppThumbnailURL":"/images/brand/favicon.png", // Thumbnail Of the App Logo
+    "favIconURL": "/images/favicon.png", // Fav Icon URL
+    "pubKey": "", // PubNub Pub Key
+    "subKey": "", // PubNub Sub Key
+    "PubNubSecret":"", // PubNub Secret
+};
